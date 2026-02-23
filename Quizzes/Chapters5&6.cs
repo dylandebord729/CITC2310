319eb1339bff46739ee73352fd9fbb5075ba7ed9
Dylan DeBord <91545068+dylandebord729@users.noreply.github.com>
2026-02-23 03:50:21 +0000

    Chapters 5 & 6 quiz

---
+++ Quizzes/Chapters5&6.cs
@@ -0,0 +1,18 @@
+//Numbers 1-6
+No Coding
+
+//number6
+{controller=Home}/{action=Index}/{id?}/{slug?}
+{controller=Home}/{action=Index}/{id?}
+{controller=Customer}/{action=List}/{id?}/{slug?}
+{controller=Customer}/{action=List}/{id?}
+
+//number7
+{controller}/{action}/{cat}/Page{num}/{sort?}
+
+{cat}
+Page
+Page{num}
+{sort?}
+/*Questions8-9
+No Coding
